207285be8a4a9442c6467bc3d5660e77e4165388
Zola McAdie <zolamcadie@gmail.com>
2024-12-10 16:27:19 -0500

    update components index import path structure

--- components/index.js
+++ components/index.js
@@ -1,3 +1,3 @@
-import "/components/header-nav/header-nav.js"
-import "/components/repository-container/repository-container.js"
-import "/components/subtopic-preview/subtopic-preview.js"
+import "./header-nav/header-nav.js"
+import "./repository-container/repository-container.js"
+import "./subtopic-preview/subtopic-preview.js"
